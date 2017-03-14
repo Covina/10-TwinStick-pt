84a671b4aad4cc07354d8cd39e3c6579302cd772
Covina <jabs303@gmail.com>
2017-03-14 08:23:55 -0700

    Test script added.

---
+++ Assets/Scripts/sjtest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sjtest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("You found the test script!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
