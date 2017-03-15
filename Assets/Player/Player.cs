4e2adf26e897450c101f4a0db5507e5d77e22439
Covina <jabs303@gmail.com>
2017-03-14 22:57:06 -0700

    Setup of Rollerball; Initial steps toward replay system

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -4,13 +4,19 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
+
+	private Input input;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		Debug.Log("H: " + CrossPlatformInputManager.GetAxis("Horizontal") );	
+		Debug.Log("V: " + CrossPlatformInputManager.GetAxis("Vertical") );	
+
 	}
 }
---
+++ Assets/Player/ReplaySystem.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplaySystem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// Sample constructor
+		MyKeyFrame testKey = new MyKeyFrame(1.0f, Vector3.up, Quaternion.identity);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
+// try as a struct vs class
+public struct MyKeyFrame {
+
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	// Constructor
+	public MyKeyFrame(float t, Vector3 pos, Quaternion rot) 
+	{
+		// Set Values for use in "new MyKeyFrame()"
+		frameTime = t;		// time
+		position = pos;		// position
+		rotation = rot;		// rotation
+
+	}
+
+
+
+}
